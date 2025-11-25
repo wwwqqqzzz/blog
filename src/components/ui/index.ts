714883db3df5e7563ab7084f4a2f98538278abb4
wwwqqqzzz <2158588419@qq.com>
2025-11-25 14:42:09 +0800

    chore: purge unused src files (magicui/ui/utils); keep theme+routes intact

--- src/components/ui/index.ts
+++ src/components/ui/index.ts
@@ -6,15 +6,12 @@ export * from './input'
 
 // 导出动画组件
 export * from './animations'
-export * from './scroll-animations'
+ 
 
 // 导出主题组件
 export * from './theme-toggle'
-export * from './immersive-toggle'
-export * from './reading-preferences'
 export * from './reading-progress-bar'
-export * from './readable-image'
-export * from './optimized-image'
+ 
 
 // 导出额外的装饰组件
 export * from './doodle-decoration'
